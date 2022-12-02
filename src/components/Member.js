23664b4d0ef196cde11e540b90d5aca6e519cb44
naoya25 <onaoya2002@gmail.com>
2022-12-02 10:40:52 +0900

    メンバーページのいらんやつ消した

--- src/components/Member.js
+++ src/components/Member.js
@@ -6,89 +6,20 @@ import img6 from "../images/image6.jpg"
 const Member = () => {
   return (
     <>
-      <div className="member-main-box">
-        <img id='mem-image' src={img4} alt="No Image" />
-      </div>
-      <div className="mem-title">VISION</div>
-      <div className="member-vision-box">
-        <div className="mem-images">
-          <img src={img6} alt="No Image" />
-          <img src={img4} alt="No Image" />
-          <img src={img4} alt="No Image" />
-          <img src={img6} alt="No Image" />
-        </div>
-        <div className="mem-vision-text">
-          DJの楽しさを広めたいという思いで活動しています。
-          本団体は、学生生活の充実ならびに学生間の親睦をはかり、DJや音楽を通じて自己を錬磨し、見識を広めることを目的としています。
-          DJは世間に知られるようになってまだ日が浅いです。 そこで学生の趣味の１つとしてDJとの繋がりを持ってもらえるように情報発信やイベントづくりに取り組んでいます。
-          将来的にはDJが趣味の人を増やし、 気軽にDJができる環境を作っていきたいと思っています！
-        </div>
-      </div>
-      <div className="mem-title">MEMBER</div>
-      <div className="mem-profiles">
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">
-            部長 内野光佳莉(DJ No-va) <br />
-            部長のひかりです！普段はDJ No-vaとして、ジャンル問わず様々なイベントに出演しています！また、プログラミングスクールでのメンターや、ダンスもしています！ <br />
-            DJサークルは、音楽好きの方、DJに興味がある方、誰でも入部大大大歓迎です！是非、SNSのDMに気軽に連絡いただけると嬉しいです❤︎ <br />
-            <a href="https://instagram.com/dj__no_va?igshid=YmMyMTA2M2Y=" target='_blank'>DJインスタ</a><br />
-            <a href="https://instagram.com/hikarinrin_____?igshid=YmMyMTA2M2Y=" target='_blank'>個人インスタ</a><br />
-          </div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-        <div className="profile">
-          <div className="pro-image"><img src={img6} alt="No Image" /></div>
-          <div className="pro-text">調整中</div>
-        </div>
-      </div>
+      <img id='mem-image' src={img4} alt="No Image" />
+      VISION
+      <img src={img4} alt="No Image" />
+      DJの楽しさを広めたいという思いで活動しています。
+      本団体は、学生生活の充実ならびに学生間の親睦をはかり、DJや音楽を通じて自己を錬磨し、見識を広めることを目的としています。
+      DJは世間に知られるようになってまだ日が浅いです。 そこで学生の趣味の１つとしてDJとの繋がりを持ってもらえるように情報発信やイベントづくりに取り組んでいます。
+      将来的にはDJが趣味の人を増やし、 気軽にDJができる環境を作っていきたいと思っています！
+      MEMBER
+      <img src={img6} alt="No Image" />
+      部長 内野光佳莉(DJ No-va) <br />
+      部長のひかりです！普段はDJ No-vaとして、ジャンル問わず様々なイベントに出演しています！また、プログラミングスクールでのメンターや、ダンスもしています！
+      DJサークルは、音楽好きの方、DJに興味がある方、誰でも入部大大大歓迎です！是非、SNSのDMに気軽に連絡いただけると嬉しいです❤︎ <br />
+      <a href="https://instagram.com/dj__no_va?igshid=YmMyMTA2M2Y=" target='_blank'>DJインスタ</a><br />
+      <a href="https://instagram.com/hikarinrin_____?igshid=YmMyMTA2M2Y=" target='_blank'>個人インスタ</a><br />
     </>
   )
 }
